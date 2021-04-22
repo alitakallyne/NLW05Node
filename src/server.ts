94c1189f401741210cf47c76b42d84bbddd87434
alitakallyne <alytakallyne@gmail.com>
2021-04-22 09:13:14 -0300

    aula02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import express from "express";
+import "./database";
+import {routes} from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(3333, ()=> console.log('Server is running on port 3333'));
-
-app.get('/', (req, res) => {
-    return res.json({message: 'Olá NLW 5'});
-})
-
-app.post('/users', (req, res) => {
-    return res.json({message: 'Usuário salvo com sucesso'});
-});
